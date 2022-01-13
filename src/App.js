f27e3b25e220d4095ef0e273256dfda76e64dfd2
StefanGreve <greve.stefan@outlook.jp>
2022-01-13 18:59:15 +0100

    Refactor code using a loop

--- src/App.js
+++ src/App.js
@@ -1,37 +1,41 @@
+import React, { useState } from "react";
 import Expenses from "./components/Expenses/Expenses";
 import NewExpense from "./components/NewExpense/NewExpense";
 
+const INITIAL_EXPENSES = [
+  {
+    id: "e1",
+    title: "Car Insurance1",
+    amount: 294.67,
+    date: new Date(2022, 2, 28),
+  },
+  {
+    id: "e2",
+    title: "Car Insurance2",
+    amount: 314.67,
+    date: new Date(2022, 2, 28),
+  },
+  {
+    id: "e3",
+    title: "Car Insurance3",
+    amount: 324.67,
+    date: new Date(2022, 2, 28),
+  },
+  {
+    id: "e4",
+    title: "Car Insurance4",
+    amount: 334.67,
+    date: new Date(2022, 2, 28),
+  },
+];
+
 function App() {
-  const expenses = [
-    {
-      id: "e1",
-      title: "Car Insurance1",
-      amount: 294.67,
-      date: new Date(2022, 2, 28),
-    },
-    {
-      id: "e2",
-      title: "Car Insurance2",
-      amount: 314.67,
-      date: new Date(2022, 2, 28),
-    },
-    {
-      id: "e3",
-      title: "Car Insurance3",
-      amount: 324.67,
-      date: new Date(2022, 2, 28),
-    },
-    {
-      id: "e4",
-      title: "Car Insurance4",
-      amount: 334.67,
-      date: new Date(2022, 2, 28),
-    },
-  ];
+  const [expenses, setExpenses] = useState(INITIAL_EXPENSES);
 
   const addExpenseHandler = (expense) => {
-    console.log("In App.js");
-    console.log(expense);
+    setExpenses((prevExpenses) => {
+      return [expense, ...prevExpenses];
+    });
   };
 
   return (
